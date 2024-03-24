4177a037f4c0a3501b98e8e6e142d5fc0af5f5b9
Sporty9Coder <126346243+Sporty9Coder@users.noreply.github.com>
2024-03-24 15:28:31 +0530

    Deploying

--- Config/dbConfig.js
+++ Config/dbConfig.js
@@ -1,2 +1,3 @@
-const dburl="mongodb://127.0.0.1:27017/BCEMERN2024";
+// const dburl="mongodb://127.0.0.1:27017/BCEMERN2024";
+const dburl="mongodb+srv://bansalrehan731:TESTtest123@g2cmern.cxyid8a.mongodb.net/BCEMERN2024?retryWrites=true&w=majority&appName=g2cmern"
 module.exports={dburl};
